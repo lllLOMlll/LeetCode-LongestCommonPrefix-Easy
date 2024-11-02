0653c0a88b629a2041468a8ec71eba97f8d1b6ea
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-01 21:37:25 -0400

    Add some tests

--- LongestCommonPrefix/Program.cs
+++ LongestCommonPrefix/Program.cs
@@ -1,2 +1,28 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿class Program 
+{
+     public string LongestCommonPrefix(string[] strs) {
+        
+        return "Hello";
+    }
+
+static void Main(string[] args)
+{
+      Program p = new Program();
+
+        // Example 1
+        string[] input1 = { "flower", "flow", "flight" };
+        string output1 = p.LongestCommonPrefix(input1);
+        Console.WriteLine("Example 1:");
+        Console.WriteLine($"Input: strs = [\"flower\", \"flow\", \"flight\"]");
+        Console.WriteLine($"Output: \"{output1}\"");
+        Console.WriteLine();
+
+        // Example 2
+        string[] input2 = { "dog", "racecar", "car" };
+        string output2 = p.LongestCommonPrefix(input2);
+        Console.WriteLine("Example 2:");
+        Console.WriteLine($"Input: strs = [\"dog\", \"racecar\", \"car\"]");
+        Console.WriteLine($"Output: \"{output2}\"");
+
+}
+}
